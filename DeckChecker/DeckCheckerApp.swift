324c59f406423ae1224ead33f888c56c79d0ee56
Leon Ma <1886818+leonmtg@users.noreply.github.com>
2023-05-09 16:48:56 +0800

    Initial Commit

---
+++ DeckChecker/DeckCheckerApp.swift
@@ -0,0 +1,17 @@
+//
+//  DeckCheckerApp.swift
+//  DeckChecker
+//
+//  Created by Leon on 2023/5/9.
+//
+
+import SwiftUI
+
+@main
+struct DeckCheckerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
