4a02e43c74d0091779aa19eeeb58e347b27571e8
Leon Ma <1886818+leonmtg@users.noreply.github.com>
2023-05-13 19:39:44 +0800

    Make error handling more reasonable

--- DeckChecker/Cards/CardsOO.swift
+++ DeckChecker/Cards/CardsOO.swift
@@ -8,28 +8,25 @@
 import SwiftUI
 import Combine
 
-struct AnkiError: Error {
+struct ErrorForView: Error, Identifiable {
+  let id = UUID()
   var message = ""
 }
 
 class CardsOO: ObservableObject {
   private var deckName: String?
-  
-  @Published private var cardIDs: CardIDs = CardIDs.empty
-  
+    
   @Published public var cards: [Card] = []
-  @Published public var error: Error?
-  
+  @Published public var errorForView: ErrorForView?
   @Published public var fetching = false
   
   private let ankiService: AnkiServiceDataPublisher
-  
   private var cancellable: AnyCancellable?
   
   public init(ankiService: AnkiServiceDataPublisher = AnkiService()) {
     self.ankiService = ankiService
     
-    $cards.combineLatest($error) { (cards, error) in
+    $cards.combineLatest($errorForView) { (cards, error) in
       return cards.isEmpty && error == nil
     }
     .assign(to: &$fetching)
@@ -40,26 +37,42 @@ class CardsOO: ObservableObject {
     
     cancellable = ankiService.cardsPublisher(for: deckName)
       .decode(type: CardIDs.self, decoder: JSONDecoder())
-      .mapError { $0 as Error }
-      .flatMap { [unowned self] cardIDs -> AnyPublisher<CardInfos, Error> in
-        self.ankiService.cardInfosPublisher(for: cardIDs.ids)
-          .decode(type: CardInfos.self, decoder: JSONDecoder())
-          .eraseToAnyPublisher()
-        
+      .map { [unowned self] cardIDs in
+        if cardIDs.ids.count > 0 {
+          return self.ankiService.cardInfosPublisher(for: cardIDs.ids)
+            .decode(type: CardInfos.self, decoder: JSONDecoder())
+            .eraseToAnyPublisher()
+        } else {
+          if let error = cardIDs.error {
+            return Fail(error: ErrorForView(message: error))
+              .eraseToAnyPublisher()
+          } else {
+            return Fail(error: ErrorForView(message: "Unknown Anki Error!"))
+              .eraseToAnyPublisher()
+          }
+        }
+      }
+      .switchToLatest()
+      .mapError { error in
+        if let errorForView = error as? ErrorForView {
+          return errorForView
+        } else {
+          return ErrorForView(message:error.localizedDescription)
+        }
       }
       .receive(on: DispatchQueue.main)
       .sink { [unowned self] completion in
         if case .failure(let error) = completion {
-          print(error)
-          self.error = error
+          self.errorForView = error
         }
       } receiveValue: { cardInfo in
         if let error = cardInfo.error {
-          self.error = AnkiError(message: error)
+          self.errorForView = ErrorForView(message: error)
         } else {
           self.cards = cardInfo.cards
           print(self.cards.first!)
         }
       }
+    
   }
 }
--- DeckChecker/Decks/DecksOO.swift
+++ DeckChecker/Decks/DecksOO.swift
@@ -19,19 +19,8 @@ class DecksOO: ObservableObject {
   public func fetchDecks() {
     ankiService.decksPublisher()
       .decode(type: Decks.self, decoder: JSONDecoder())
-      .mapError({ error -> Error in
-        print(error)
-        return error
-      })
       .replaceError(with: Decks.errorDecks)
       .receive(on: DispatchQueue.main)
-      .handleEvents(receiveSubscription: { _ in
-        print("Network request will start")
-      }, receiveOutput: { data in
-        print("Network request data received: \(data)")
-      }, receiveCancel: {
-        print("Network request cancelled")
-      })
       .assign(to: &$decks)
   }
 }
